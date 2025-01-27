43fae7488c94979b415aea842a6826aaa7e11b62
Reynaldo Jr Baja <reynaldobaja2@gmail.com>
2025-01-27 17:36:21 +0800

    backendv1

--- database/migrations/2025_01_27_092943_create_projects_table.php
+++ database/migrations/2025_01_27_092943_create_projects_table.php
@@ -13,16 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('role');
-            $table->string('email')->unique();
-            $table->string('phone');
-            $table->string('address');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('projects');
     }
 };
