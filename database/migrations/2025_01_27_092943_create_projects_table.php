98b1d2d7402f64a54afd4460bd80937bbb302610
Reynaldo Jr Baja <reynaldobaja2@gmail.com>
2025-01-27 17:47:18 +0800

    project_migration

--- database/migrations/2025_01_27_092943_create_projects_table.php
+++ database/migrations/2025_01_27_092943_create_projects_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('project_name');
             $table->timestamps();
         });
     }
